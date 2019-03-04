0ab37d38e8ec356b4d8aac0491d185c8a79710c4
Jackie <jacgut19@gmail.com>
2019-03-04 20:18:23 +0000

    chaining jquery that just speed up the code production

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ $("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
 
 // chaining - that just speed up the code production
 // it has the ability to chain together multipule functions or methods on a particular
-// set of jquery objectsor element that we grab from the page in one line of code and 
+// set of jquery objects or element that we grab from the page in one line of code and 
 // chaining them together using a dot syntax
 
 $("#contact-methods").css({border: "2px solid red"})
