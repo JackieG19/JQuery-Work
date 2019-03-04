56ec6de0b540715a789d8dbf244f57c077b96008
Jackie <jacgut19@gmail.com>
2019-03-04 16:29:25 +0000

    using jquery method to grab the heading and change it's properties

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 $(document).ready(function(){
     alert("working with jquery");
 });
+
+// grabing the h1 tag with the id of logo
+$("logo");
+
+var heading = $("logo");
+
+// jquery method in changig the css property of the heading
+heading.css({position: "relative"})
+
+// animation method is going to move the heading 100 pixels from the left
+heading.animation({left: 100})
