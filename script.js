ae1a612f9ea2d507f6b15e90e05ec4046ba53785
Jackie <jacgut19@gmail.com>
2019-03-04 16:58:33 +0000

    grabbing an element in a jquery wrapper and manipulate it further

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ heading.css({position: "relative"})
 
 // animation method is going to move the heading 100 pixels from the left
 heading.animation({left: 100})
+
+// grabbing an element in a jquery wrapper and manipulate it further
+$("#lead-banner p");
+
+// element selectors
+$("h3").css({border: "3xp solid blue"});
+
+// class selectors
+$(".wapper").css({border: "3xp solid red"});
+
+// ID selectors
+$("#clients").css({border: "3xp solid yellow"});
