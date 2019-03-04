8a78185ea94270476b12217ebffc6ea23e41ea9a
Jackie <jacgut19@gmail.com>
2019-03-04 17:28:05 +0000

    using filter to refine selectors

--- script.js
+++ script.js
@@ -26,3 +26,32 @@ $(".wapper").css({border: "3xp solid red"});
 
 // ID selectors
 $("#clients").css({border: "3xp solid yellow"});
+
+// filter can be used to refine selectors to make them more specific very much 
+// like css psuedo classes. 
+
+// first and last
+$("header nav li:first").css({border: "2px solid red"});
+$("header nav li:last").css({border: "2px solid red"});
+
+// first-child and last-child
+$("#contact ul:first-child").css({border: "2px solid red"});
+$("#contact ul:last-child").css({border: "2px solid red"});
+
+// even and odd
+$("header nav li:even").css({border: "2px solid blue"});
+$("header nav li:odd").css({border: "2px solid yellow"});
+
+// not
+$("#section:not ('#contact')").css({border: "2px solid green"});
+
+// lesser than and greater than
+$("#social-nav li:lit(3)").css({border: "2px solid blue"});
+$("#social-nav li:lit(2)").css({border: "2px solid blue"});
+
+// attribute filters
+$("div[class]").css({border: "2px solid pink"});
+$("img[alt]").css({border: "2px solid pink"});
+
+$("img[alt=quote]").css({border: "2px solid purple"});
+
